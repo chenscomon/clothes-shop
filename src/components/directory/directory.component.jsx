78750031a8dfa2c60823f326de02826ea9a6ac74
chenscomon <63293481+chenscomon@users.noreply.github.com>
2020-08-22 11:06:47 +0300

    Dividing homepage to components and styling

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import './directory.styles.scss';
+import MenuItem from '../menu-item/menu-item.component';
+
+class Directory extends Component {
+    constructor(){
+        super();
+
+        this.state = {
+            sections: [
+                {
+                    title: 'hats',
+                    imageUrl: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+                    id: 1
+                },
+                {
+                    title: 'jackets',
+                    imageUrl: 'https://images.unsplash.com/photo-1544022613-e87ca75a784a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80',
+                    id: 2
+                },
+                {
+                    title: 'sneakers',
+                    imageUrl: 'https://images.unsplash.com/photo-1552346154-21d32810aba3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+                    id: 3
+                },
+                {
+                    title: 'women',
+                    imageUrl: 'https://images.unsplash.com/photo-1552664199-fd31f7431a55?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80',
+                    id: 4,
+                    size: 'large'
+                },
+                {
+                    title: 'men',
+                    imageUrl: 'https://images.unsplash.com/photo-1551794840-8ae3b9c181f8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80',
+                    id: 5,
+                    size: 'large'
+                }
+            ]
+        }
+    }
+    render() {
+        return (
+            <div className="directory-menu">
+                {this.state.sections.map((section) => <MenuItem size={section.size} title={section.title} key={section.id} imageUrl={section.imageUrl} />)}
+        </div>
+        )
+    }
+}
+
+export default Directory;
