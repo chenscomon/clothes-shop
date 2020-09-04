39e81d6cd0248d5ac22ffaf7641dc1c10571d5b8
chenscomon <63293481+chenscomon@users.noreply.github.com>
2020-09-04 17:54:16 +0300

    finish firebase sign-in-and-up

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 class SignIn extends Component {
     constructor(props){
@@ -14,12 +14,19 @@ class SignIn extends Component {
         }
     }
 
-    handleSubmit = (e) => {
+    handleSubmit = async (e) => {
         e.preventDefault();
-        this.setState({
-            email: '',
-            password: ''
-        });
+        const { email, password } = this.state;
+        try {
+            await auth.createUserWithEmailAndPassword(email, password);
+            this.setState({
+                email: '',
+                password: ''
+            });
+        } catch (err) {
+            console.log(err);
+        }
+        
     }
 
     handleChange = e => {
