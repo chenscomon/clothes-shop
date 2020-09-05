7d5510615e221a6a7cf378d4d2a92b86cf3dc265
chenscomon <63293481+chenscomon@users.noreply.github.com>
2020-09-05 18:49:34 +0300

    Adding hidden state to the cart reducer for the cart list component and adding quantity for the state

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -6,8 +6,8 @@ const CollectionPreview = ({ title, items }) => (
     <div className="collection-preview">
         <h1 className="title">{title.toUpperCase()}</h1>
         <div className="preview">
-            {items.filter((item, index) => index < 4).map(({id, ...itemProps}) => (
-                <CollectionItem key={id} {...itemProps} />
+            {items.filter((item, index) => index < 4).map((item) => (
+                <CollectionItem item={item} key={item.id} />
             ))}
         </div>
     </div>
